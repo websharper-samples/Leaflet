8551f8d45972cc1076b1787c05c75e13adf7d796
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-09-06 20:34:24 +0200

    Initial commit for Leaflet

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WebSharper @sampleName@
+# WebSharper Leaflet
 
-Sample for [WebSharper](https://websharper.com) bindings for @libLink@.
+Sample for [WebSharper](https://websharper.com) bindings for [Leaflet](https://leafletjs.com/).
 
-* [Source repository on GitHub](@repoUrl@)
-* [See it running live](@liveUrl@)
+* [Source repository on GitHub](https://github.com/websharper-samples/Leaflet)
+* [See it running live](https://websharper-samples.github.io/Leaflet)
--- src/Client.fs
+++ src/Client.fs
@@ -1,1 +1,39 @@
-TODO Replace me
+namespace WebSharper.Leaflet.Tests
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.UI
+open WebSharper.UI.Html
+open WebSharper.UI.Client
+open WebSharper.UI.Notation
+
+[<JavaScript>]
+module Client =
+
+    [<SPAEntryPoint>]
+    let Main () =
+        let coordinates = Var.Create ""
+        div [] [
+            div [
+                attr.style "height: 600px;"
+                on.afterRender (fun div ->
+                    let map = Leaflet.Map(div)
+                    map.SetView((47.49883, 19.0582), 14)
+                    map.AddLayer(
+                        Leaflet.TileLayer(
+                            Leaflet.TileLayer.OpenStreetMap.UrlTemplate,
+                            Leaflet.TileLayer.Options(
+                                Attribution = Leaflet.TileLayer.OpenStreetMap.Attribution)))
+                    map.AddLayer(
+                        let m = Leaflet.Marker((47.48553, 19.07153))
+                        m.BindPopup("IntelliFactory")
+                        m)
+                    map.On_mousemove(fun map ev ->
+                        coordinates := "Position: " + ev.Latlng.ToString())
+                    map.On_mouseout(fun map ev ->
+                        coordinates := "")
+                )
+            ] []
+            div [] [text coordinates.V]
+        ]
+        |> Doc.RunById "main"
